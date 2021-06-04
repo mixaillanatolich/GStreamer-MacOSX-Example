1a91c334e2319f06767176b64c714ef81d1bb3c9
Mikhail Muravev <mixaillanatolich@gmail.com>
2021-06-04 13:12:49 +0300

    app template

---
+++ GStreamer MacOSX Example/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GStreamer MacOSX Example
+//
+//  Created by Mixaill on 04.06.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GStreamer MacOSX Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GStreamer MacOSX Example
+//
+//  Created by Mixaill on 04.06.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
